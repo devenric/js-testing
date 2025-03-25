c7a0969bcf2551201b98176fdc2c9603f1d829ac
devenric <engato@alumnatflorida.es>
2025-03-25 13:11:08 +0100

    Generar texto V1

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ const dia = fecha.getMinutes();
 console.log(`${date}/${mes}/${año}`);
 console.log("hola");
 
+let user = prompt("cual es tu nombre?");
 const content = document.querySelector('.content');
 let respuesta = prompt("rojo o azul?");
 if (respuesta == "rojo") {
     content.style.backgroundColor = 'red';
 }
 else {content.style.backgroundColor = 'blue';}
+
+let respuesta2 = prompt("quieres crear un texto?");
+if (respuesta2 == "si") {
+    let texto = prompt("escribe el texto");
+    document.createElement('p');
+    texto.className = 'nuevoelemento';
+    texto.textContent = texto;
+    let newelement = document.querySelector('.texto');
+    content.appendChild(newelement);
+}
+else {confirm(`adios,${user}`);}
