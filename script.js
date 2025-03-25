b4a37a80a48dbbf83b4ff63b7c9e16a972ae64b2
devenric <engato@alumnatflorida.es>
2025-03-25 11:53:37 +0100

    date fixed v4

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let respuesta = prompt("Introduce tu nombre")
 const fecha = new Date()
 const date = fecha.getDate()
 const año = fecha.getFullYear()
 const mes = fecha.getMonth() + 1
 const dia = fecha.getMinutes()
-console.log(`${date}/${mes}/${año}`)
+console.log(`${date}/${mes}/${año} ${fecha.getHours()}:${dia}`)
+console.log("hola")
